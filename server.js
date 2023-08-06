32d0547b17f02b71517bc85a4c9d1fc18097d717
kirstianc <48222417+kirstianc@users.noreply.github.com>
2023-08-06 16:44:39 -0700

    updated server

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express');
-
 const app = express();
-
 const path = require('path');
-
 const port = process.env.PORT || 5000;
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('build'));
-    app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-    })
-}
+// Serve the static files from the "build" directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Send the "index.html" file for any other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, (err) => {
-    if(err){
-        return console.log(err);
-    }
-    console.log('Server running on port ', port);
-})
+  if (err) {
+    return console.log(err);
+  }
+  console.log('Server running on port ', port);
+});
